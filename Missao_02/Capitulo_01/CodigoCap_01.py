fb9a8b7365b5bc5aa39446b4500a87f042c24897
Yalle Rocha <yalle.rocha2020@gmail.com>
2023-03-29 19:58:31 -0300

    .

---
+++ Missao_02/Capitulo_01/CodigoCap_01.py
@@ -0,0 +1,16 @@
+# Importação das bibliotecas
+import cv2
+# Leitura da imagem com a função imread()
+imagem = cv2.imread('Entrada.jpg')
+print('Largura em pixels: ', end='')
+print(imagem.shape[1]) #largura da imagem
+print('Altura em pixels: ', end='')
+print(imagem.shape[0]) #altura da imagem
+print('Qtde de canais: ', end='')
+print(imagem.shape[2])
+# Mostra a imagem com a função imshow
+cv2.imshow("Nome da janela", imagem)
+cv2.waitKey(0) #espera pressionar qualquer tecla
+# Salvar a imagem no disco com função imwrite()
+cv2.imwrite("saida.jpg", imagem)
+
