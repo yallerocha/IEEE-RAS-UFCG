05d8db3628dd2634952681dacd5188319075060c
Yalle Rocha <yalle.silva@ccc.ufcg.edu.br>
2023-03-30 15:32:05 -0300

    .

--- Missao_01/Codigo.ino
+++ Missao_01/Codigo.ino
@@ -1,14 +1,20 @@
 const byte LED = 13;
 
+/* 
+Define o pino escolhido como saída de informações.
+*/
 void setup(){
   pinMode(LED, OUTPUT);
 }
 
+/* 
+Um loop que mantem o prejeto em funcionamento chamando a função
+blink três vezes com períodos diferentes de tempo.
+*/
 void loop(){
   blink(LED, 2000, 6000);
   blink(LED, 1000, 6000);
   blink(LED, 500, 6000);
-  return;
 }
 
 /* 
