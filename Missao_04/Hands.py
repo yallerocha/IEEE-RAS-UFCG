fb2819a2c5af7f7c34c803e7e24ce2f2533812be
Yalle Rocha <yalle.silva@ccc.ufcg.edu.br>
2023-04-20 18:22:51 -0300

    Update Hands.py

--- Missao_04/Hands.py
+++ Missao_04/Hands.py
@@ -1,66 +1,77 @@
 import cv2
 import mediapipe as mp
 
+def main():
+    handNumbers = HandNumbers()
+    handNumbers.start(0)
+
 class HandNumbers:
         
-    def start(self, camIndex):
-        video = cv2.VideoCapture(camIndex)
+    def start(self, cam):
+        video = cv2.VideoCapture(cam)
 
         hands = mp.solutions.hands
-        Hands = hands.Hands(max_num_hands=2)
+        Hands = hands.Hands(max_num_hands = 2)
         mpDwaw = mp.solutions.drawing_utils
 
         while True:
             success, frame = video.read()
             frameRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
             results = Hands.process(frameRGB)
-            height, width, _  = frame.shape
-            handsLandmarks = results.multi_hand_landmarks 
+            handsLandmarks = results.multi_hand_landmarks
 
-            if handsLandmarks:
-                raisedFingers = 0
+            if handsLandmarks != None:
+                totalOpenFingers = 0
+                
                 for Landmarks in handsLandmarks:
-                    keyPoints = []
-                    for cord in Landmarks.landmark:
-                        world_x, world_y = int(cord.x * width), int(cord.y * height)
-                        keyPoints.append((world_x, world_y))
+                    worldKeyPoints = self.__worldKeyPoints(frame, Landmarks)
                     
-                    if self.__handUp(keyPoints):
+                    if self.__handUp(worldKeyPoints) == True:
                         mpDwaw.draw_landmarks(frame, Landmarks, hands.HAND_CONNECTIONS)
-                        
-                        fingersTips = [8, 12, 16, 20]         
-                        for tip in fingersTips:
-                            if self.__fingerUp(keyPoints,tip):
-                                raisedFingers += 1
-                    
-                        if keyPoints[4][0] < keyPoints[17][0]:
-                            if keyPoints[4][0] < keyPoints[3][0]:
-                                raisedFingers += 1
-                        else:
-                            if keyPoints[4][0] > keyPoints[3][0]:
-                                raisedFingers += 1
-
-                cv2.rectangle(frame, (80, 10), (300, 110), (255, 255, 255), -1)
-                cv2.putText(frame, str(raisedFingers), (100, 100), 
-                            cv2.FONT_HERSHEY_DUPLEX, 4, (0, 0, 255), 5)
+                        totalOpenFingers += self.__fingersOpen(worldKeyPoints)
+                            
+                self.__digitalDisplay(frame, totalOpenFingers)
 
             cv2.imshow('Frame', frame)
             cv2.waitKey(1)
+            
+    def __worldKeyPoints(self, frame, Landmarks):
+        height, width, _  = frame.shape
+        worldKeyPoints = []
+        
+        for cord in Landmarks.landmark:
+            x, y = int(cord.x * width), int(cord.y * height)
+            worldKeyPoints.append((x, y))
+        
+        return worldKeyPoints
 
     def __handUp(self, keyPoints):
-            if keyPoints[0][1] < keyPoints[2][1]:
-                return False
-            elif keyPoints[1][1] < keyPoints[17][1]:
-                return False
-            else:
-                return True
-
-    def __fingerUp(self, keyPoints, tip):
-        if keyPoints[tip][1] < keyPoints[tip-2][1]:
-            return True
-        else:
+        if keyPoints[0][1] < keyPoints[2][1]:
             return False
-        
-a = HandNumbers()
-a.start(0)
+        elif keyPoints[1][1] < keyPoints[17][1]:
+            return False
+        else:
+            return True
 
+    def __fingersOpen(self, keyPoints):
+        fingersOpen = 0
+        
+        fingersTips = [8, 12, 16, 20]     
+        for tip in fingersTips:
+            if keyPoints[tip][1] < keyPoints[tip-2][1]:
+                fingersOpen += 1
+        
+        if keyPoints[4][0] < keyPoints[17][0]:
+            if keyPoints[4][0] < keyPoints[3][0]:
+                fingersOpen += 1
+        elif keyPoints[4][0] > keyPoints[3][0]:
+            fingersOpen += 1
+                
+        return fingersOpen
+    
+    def __digitalDisplay(self, frame, totalOpenFingers):
+        cv2.rectangle(frame, (0, 0), (220, 120), (0, 200, 0), -1)
+        cv2.putText(frame, str(totalOpenFingers), (20, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (255, 255, 255), 5)
+
+if __name__ == "__main__":
+    main()
