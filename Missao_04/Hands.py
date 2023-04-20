c6d7fbb08d6f7e5fbc025509b72e154849861812
Yalle Rocha <yalle.silva@ccc.ufcg.edu.br>
2023-04-20 02:28:41 -0300

    Update Hands.py

--- Missao_04/Hands.py
+++ Missao_04/Hands.py
@@ -1,24 +1,24 @@
 import cv2
 import mediapipe as mp
 
-video = cv2.VideoCapture(0)
+webCam = cv2.VideoCapture(0)
 
 hands = mp.solutions.hands
 Hands = hands.Hands(max_num_hands=2)
 mpDwaw = mp.solutions.drawing_utils
 
 while True:
-    success, img = video.read()
+    success, img = webCam.read()
     frameRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     results = Hands.process(frameRGB)
     handPoints = results.multi_hand_landmarks
     h, w, _ = img.shape
-    pontos = []
+
     if handPoints:
+        contador = 0
         for points in handPoints:
-            print(points)
             mpDwaw.draw_landmarks(img, points, hands.HAND_CONNECTIONS)
-            
+            pontos = []
             #podemos enumerar esses pontos da seguinte forma
             for id, cord in enumerate(points.landmark):
                 cx, cy = int(cord.x * w), int(cord.y * h)
@@ -26,21 +26,20 @@ while True:
                 pontos.append((cx,cy))
 
             dedos = [8,12,16,20]
-            contador = 0
             if pontos:
                 if pontos[4][0] < pontos[17][0]:
-                    if pontos[4][0] < pontos[3][0]:
+                    if pontos[4][0] < pontos[2][0]:
                         contador += 1
                 else:
-                    if pontos[4][0] > pontos[3][0]:
+                    if pontos[4][0] > pontos[2][0]:
                         contador += 1  
                 for x in dedos:
-                   if pontos[x][1] < pontos[x-2][1]:
-                      contador +=1
+                    if pontos[x][1] < pontos[x-2][1]:
+                        contador +=1
 
-            cv2.rectangle(img, (80, 10), (200,110), (255, 0, 0), -1)
-            cv2.putText(img,str(contador),(100,100),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),5)
-            #print(contador)
+        cv2.rectangle(img, (80, 10), (200,110), (255, 0, 0), -1)
+        cv2.putText(img,str(contador),(100,100),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),5)
+            
 
     cv2.imshow('Imagem',img)
     cv2.waitKey(1)
