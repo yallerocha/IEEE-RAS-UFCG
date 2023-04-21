67697c767b6161d66ddcff2d6b9d52f3b726afc9
Yalle Rocha <yalle.silva@ccc.ufcg.edu.br>
2023-04-20 21:01:40 -0300

    Update HandTracking.py

--- Missao_04/HandTracking.py
+++ Missao_04/HandTracking.py
@@ -6,35 +6,52 @@ def main():
     handTracking.start(0)
 
 class HandTracking:
-        
+    
+    #Funcao que inicia o tracking das maos:   
     def start(self, cam):
+        #Variavel que guarda o endereco da camera a ser utilizada:
         video = cv2.VideoCapture(cam)
-
+        #Variavel que guarda a solution hands do MediaPipe, que possibilita trabalhar com o tracking de maos:
         hands = mp.solutions.hands
+        #Configura a solution para detectar no maximo duas maos:
         Hands = hands.Hands(max_num_hands = 2)
+        #Variavel que guarda a solution responsavel pela representação visual dos resultados do tracking:
         mpDraw = mp.solutions.drawing_utils
 
         while True:
+            #Leitura do frame do video:
             success, frame = video.read()
+            #Conversao do frame de BGR para RGB, para que possa ser processado:
             frameRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            #Variavel que guarda os resultados do processamento do frame:
             results = Hands.process(frameRGB)
+            #Variavel que guarda os landmarks de cada mao processada:
             handsLandmarks = results.multi_hand_landmarks
-
+            
+            #Verifica se houve processamento de maos no frame, caso não, o loop segue normalmente:
             if handsLandmarks != None:
+                #Variavel que guarda o numero total de dedos abertos nas maos:
                 totalOpenFingers = 0
-                
+                #Laço que itera sobre os Landmarks (Objetos) de cada mao:
                 for Landmarks in handsLandmarks:
+                    #Variavel que gurda as coordenadas globais de cada ponto de tracking da mao:
                     worldKeyPoints = self.__worldKeyPoints(frame, Landmarks.landmark)
                     
+                    #Verifica se a mao esta para cima, para garantir o funcionamento correto do codigo:
                     if self.__handUp(worldKeyPoints) == True:
+                        #Desenha os pontos e as conexoes entre os pontos de tracking no frame:
                         mpDraw.draw_landmarks(frame, Landmarks, hands.HAND_CONNECTIONS)
+                        #Atribuicao da quantidade de dedos abertos na mao analisada:
                         totalOpenFingers += self.__fingersOpen(worldKeyPoints)
-                            
+                        
+                #Adiciona um display no frame que mostra o numero total de dedos abertos:           
                 self.__digitalDisplay(frame, totalOpenFingers)
-
+            
+            #Vizualização do frame com as informações adicionadas:
             cv2.imshow('Frame', frame)
             cv2.waitKey(1)
-            
+    
+    #       
     def __worldKeyPoints(self, frame, landmark):
         height, width, _  = frame.shape
         worldKeyPoints = []
